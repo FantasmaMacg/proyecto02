658ca6655e2f98de1cd3f420f0545e22b1891f1c
MACG <curso08@nomail.com>
2024-11-04 13:34:55 +0100

    cambios calculadora

--- src/test/java/es/santander/ascender/CalculadoraTest.java
+++ src/test/java/es/santander/ascender/CalculadoraTest.java
@@ -8,10 +8,15 @@ import org.junit.jupiter.api.Test;
 public class CalculadoraTest {
 
     @Test
-    
     public void testSumar(){
         Calculadora sut = new Calculadora();
         assertTrue(sut.sumar(4, 3) == 7);
+    }
+    @Test
+    public void testSumartwo(){
+        Calculadora sut = new Calculadora();
+        int resultado = sut.sumar(4, 7);
+        assertTrue(resultado == 11);
     }
     @Test
     public void testRestar(){
