dbbc8fa8b648e6d9442a0a130ff59c00528c31f5
MACG <curso08@nomail.com>
2024-11-04 13:27:31 +0100

    Creando Calculadora

---
+++ src/main/java/es/santander/ascender/Calculadora.java
@@ -0,0 +1,17 @@
+package es.santander.ascender;
+
+public class Calculadora {
+     public int sumar(int a , int b){  
+        return a+b;
+    }
+    public int restar (int a , int b){
+         return a-b;
+    }
+    public int dividir (int a , int b){
+        return(a/b);
+    }
+    public int multiplicar (int a , int b){
+        return(a*b);
+    }
+    
+}
---
+++ src/test/java/es/santander/ascender/CalculadoraTest.java
@@ -0,0 +1,33 @@
+package es.santander.ascender;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+import org.junit.jupiter.api.Test;
+
+public class CalculadoraTest {
+
+    @Test
+    
+    public void testSumar(){
+        Calculadora sut = new Calculadora();
+        assertTrue(sut.sumar(4, 3) == 7);
+    }
+    @Test
+    public void testRestar(){
+        Calculadora sut = new Calculadora();
+        assertTrue(sut.restar(4, 3) == 1);
+    }
+    @Test
+    public void testDividir(){
+        Calculadora sut = new Calculadora();
+        assertTrue(sut.dividir(4, 2) == 2);
+    }
+    @Test
+    public void testMultiplicar(){
+        Calculadora sut = new Calculadora();
+        assertTrue(sut.multiplicar(4, 3) == 12);
+    }
+
+    
+}
